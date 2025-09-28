48824029ed7d37133564f3308d1f2ea016db4772
Joseph Yu <josephyu2712+github@gmail.com>
2025-09-28 17:51:33 +0100

    vim, no plugins

---
+++ .config/vim/README.md
@@ -0,0 +1,5 @@
+# Vim
+
+Not all servers will have nvim installed, but some will at least have vim (even in it's simplist form)
+
+Not all will be on version 9.1+, so we also create a symlink at `~/.vim`
--- install.bash
+++ install.bash
@@ -35,27 +35,64 @@ DEST_CONFIG="${XDG_CONFIG_HOME:-$HOME}/.config"
 DEST_CONFIG="$(readlink -m $DEST_CONFIG)"
 mkdir -p "$DEST_CONFIG"
 
-# Git
-DEST_GIT="$DEST_CONFIG"/git
-if [[ -L "$DEST_GIT" ]]
+# Config folder install
+install_under_home_folder() {
+    DEST_SUB_PATH="$1"
+    FROM_SUB_PATH="${2:-$DEST_SUB_PATH}"
+    DEST_DIR="$HOME"/"$DEST_SUB_PATH"
+    if [[ -e "$DEST_DIR" ]]
+    then
+        if [[ -L "$DEST_DIR" ]]
+        then
+            rm "$DEST_DIR"
+        else
+            BACKUP_DEST="$BACKUP_DIR"/"$DEST_SUB_PATH"
+            mkdir -p "$(dirname $BACKUP_DEST)"
+            mv -T "$DEST_DIR" "$BACKUP_DEST"
+            cat << EOF >> "$RESTORE_BASH"
+if [[ -L '$DEST_DIR' ]]
 then
-    rm "$DEST_GIT"
-else
-    BACKUP_DEST="$BACKUP_DIR"/.config/git
-    mkdir -p "$(dirname $BACKUP_DEST)"
-    mv -T "$DEST_GIT" "$BACKUP_DEST"
-    cat << EOF >> "$RESTORE_BASH"
-if [[ -L '$DEST_GIT' ]]
-then
-    rm '$DEST_GIT'
-    mv -vT '$BACKUP_DEST' '$DEST_GIT'
+    rm '$DEST_DIR'
+    mv -vT '$BACKUP_DEST' '$DEST_DIR'
 fi
 EOF
+        fi
+    fi
+    ln -vs "$THIS_DIR"/"$FROM_SUB_PATH" "$DEST_DIR"
+}
+install_config_folder() {
+    FOLDER_NAME="$1"
+    DEST_DIR="$DEST_CONFIG"/"$FOLDER_NAME"
+    if [[ -e "$DEST_DIR" ]]
+    then
+        if [[ -L "$DEST_DIR" ]]
+        then
+            rm "$DEST_DIR"
+        else
+            BACKUP_DEST="$BACKUP_DIR"/.config/"$FOLDER_NAME"
+            mkdir -p "$(dirname $BACKUP_DEST)"
+            mv -T "$DEST_DIR" "$BACKUP_DEST"
+            cat << EOF >> "$RESTORE_BASH"
+if [[ -L '$DEST_DIR' ]]
+then
+    rm '$DEST_DIR'
+    mv -vT '$BACKUP_DEST' '$DEST_DIR'
 fi
-ln -vs "$THIS_DIR"/.config/git "$DEST_GIT"
+EOF
+        fi
+    fi
+    ln -vs "$THIS_DIR"/.config/"$FOLDER_NAME" "$DEST_DIR"
+}
+
+# Git
+install_config_folder git
+
+# Vim
+install_config_folder vim
+install_under_home_folder .vim .config/vim
 
 # ~/.bash*
-for FILE_PATH in "$THIS_DIR"/.config/bash/*
+for FILE_PATH in "$THIS_DIR"/.config/bash/*/
 do
     FILENAME="$(basename "$FILE_PATH")"
     DEST="$HOME"/."$FILENAME"
@@ -69,9 +106,9 @@ do
     fi
     sed --in-place --file=- "$DEST" << EOF
 1i \
-source "${FILE_PATH}/prepend"
+source "${FILE_PATH}prepend"
 \$a \
-source "${FILE_PATH}/append"
+source "${FILE_PATH}append"
 EOF
     echo "Prepend and appended 'source ...' to $DEST"
 done
